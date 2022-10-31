03317da30e094840f6d3b66c35758859ae93b62a
IsmielAbir <ismielabir286@gmail.com>
2022-11-01 02:24:18 +0600

    Users part update

--- user.py
+++ user.py
@@ -1,6 +1,5 @@
 import hashlib
 
-
 class User:
     def __init__(self, name, email, password):
         self.name = name
@@ -10,9 +9,27 @@ class User:
             f.write(f'{email} {pwd}')
         f.close()
         print(self.name, 'user created')
-        
-        
+
     
     
+    @staticmethod
+    def log_in(email, password):  
+        stored_pass = ''
+        with open('users.txt', 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                if email in line:
+                    stored_pass = line.split(' ')[1]
+                      
+        f.close()
+        hash_pwd = hashlib.md5(password.encode()).hexdigest()
+        if hash_pwd == stored_pass:
+            print('Valid user')
+            return True
+        else:
+            print('Invalid user')
+            return False
+        print('Password found', stored_pass)
     
-    
+a = User('sb', 'ab@hh', 'hhjj')
+User.log_in('ab@hh', 'hhjj') 
